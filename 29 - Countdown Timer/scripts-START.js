c6a5b43a34b12a76bb02aeae8315ea8e22044f44
elikantor <eli.kantor@outlook.com>
2020-10-01 18:01:05 -0400

    add full functionality

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,14 +2,56 @@ const buttons = document.querySelectorAll('button')
 const form = document.querySelector('form')
 const timeLeft = document.querySelector('.display__time-left')
 const endTime = document.querySelector('.display__end-time')
+let timer = false
+let interval
 
-
-function startTimer() {
-    const time = Number(this.dataset.time)
-    const minutes = Math.floor(time / 60)
-    const secs = time % 60 > 9 ? time % 60 : '0' + time % 60
-    timeLeft.innerText = `${minutes}:${secs}`
+function startTimer(e, seconds) {
+    console.log(seconds, this)
+    let time = seconds ? seconds : Number(this.dataset.time)
+    let minutes = Math.floor(time / 60)
     generateEndTime(minutes)
+    let secs = time % 60 > 9 ? time % 60 : '0' + time % 60
+    timeLeft.innerText = `${minutes}:${secs}`
+    document.title = `${minutes}:${secs}`
+    if(!timer) {
+        timer = true
+        interval = setInterval(function(){
+            if(time) {
+                if(secs === '00' && minutes > 0) {
+                    minutes--
+                    secs = 59
+                } else {
+                    secs--
+                }
+                time--
+                secs = time % 60 > 9 ? time % 60 : '0' + time % 60
+                timeLeft.innerText = `${minutes}:${secs}`
+                document.title = `${minutes}:${secs}`
+            } else {
+                clearInterval(interval)
+                timer = false
+            }
+        }, 1000)
+    } else {
+        clearInterval(interval)
+        interval = setInterval(function(){
+            if(time) {
+                if(secs === '00' && minutes > 0) {
+                    minutes--
+                    secs = 59
+                } else {
+                    secs--
+                }
+                time--
+                secs = time % 60 > 9 ? time % 60 : '0' + time % 60
+                timeLeft.innerText = `${minutes}:${secs}`
+                document.title = `${minutes}:${secs}`
+            } else {
+                clearInterval(interval)
+                timer = false
+            }
+        }, 1000)
+    }
 }
 
 function generateEndTime(minutes){
@@ -19,4 +61,11 @@ function generateEndTime(minutes){
     endTime.innerText = `Be back at ${hours}:${mins}`
 }
 
+function customerTimer(e) {
+    e.preventDefault()
+    startTimer(e, this.minutes.value * 60)
+}
+
+
 buttons.forEach(button => button.addEventListener('click', startTimer))
+form.addEventListener('submit',customerTimer)
