55c3e4ce3dd7d32e23f5ae3222ca2bcd9cc32e8c
elikantor <eli.kantor@outlook.com>
2020-10-26 15:30:03 -0400

    add play/pause functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,34 @@
+const playButton = document.querySelector('.player__button')
+const video = document.querySelector('video')
+const progress = document.querySelector('.progress')
+const progress_filled = document.querySelector('.progress__filled')
+const inputs = document.querySelectorAll('input')
+let paused = false
+
+function togglePlay() {
+    if(paused) {
+        video.play()
+        playButton.innerText = '||'
+    } else {
+        video.pause()
+        playButton.innerText = '►'
+    }
+    paused = !paused
+}
+
+function handleProgressUpdate(e) {
+    progress_filled.style.width = `${(e.offsetX / this.clientWidth)*100}%`
+    progress_filled.style.flexBasis = `${(e.offsetX / this.clientWidth)*100}%`
+    console.log(video.playbackRate)
+}
+
+function handleClick(e) {
+    console.log(this)
+}
 
 
 
 
+playButton.addEventListener('click', togglePlay)
+progress.addEventListener('click', handleProgressUpdate)
+inputs.forEach(input => input.addEventListener("click", handleClick))
